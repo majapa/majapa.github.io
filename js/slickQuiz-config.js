4fb21c92031b3535c63b27aa6223c8158e0db4c8
majapa <jake@tfn.org>
2014-06-20 16:57:01 -0500

    quyiz

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -22,7 +22,7 @@ var quizJSON = {
                 {"option": "Men",      "correct": true} // no comma here
             ],
             "correct": "<p><span>Yup.</span> During a board discussion about civil rights, former SBOE chairman Don McLeroy said, &#34;For instance, the women’s right to vote. The women didn’t vote on it, the men did. The men passed it for the women.&#34;</p>",
-            "incorrect": "<p><span>The SBOE disagrees.</span> During a board discussion about civil rights, former SBOE chairman Don McLeroy said, &#34;For instance, the women’s right to vote. The women didn’t vote on it, the men did. The men passed it for the women.&#34;</p>" // no comma here
+            "incorrect": "<p><span>The SBOE disagrees.</span> During a board discussion about civil rights, former SBOE chairman Don McLeroy said, &#34;For instance, the women’s right to vote. The women didn’t vote on it; the men did. The men passed it for the women.&#34;</p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Single True Answer
             "q": "According to the SBOE, which of these three factors is the <u>least significant</u> cause of the Civil War?",
@@ -35,32 +35,32 @@ var quizJSON = {
             "incorrect": "<p><span>Nope: slavery ranks last.</span> By a deliberate vote of the board, the current social studies curriculum standards list the causes of the Civil War in the following order: states’ rights, sectionalism, and slavery.</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Fill in the blank. Former SBOE chairman Don McLeroy once instructed a panel of curriculum reviewers that infamous red-baiter Joseph McCarthy had been _______ by history.",
+            "q": "Fill in the blank: Former SBOE chairman Don McLeroy once instructed a panel of curriculum reviewers that infamous red-baiter Joseph McCarthy had been _______ by history.",
             "a": [
                 {"option": "Damned",                  "correct": false},
                 {"option": "Vindicated",           "correct": true},
                 {"option": "Forgotten",  "correct": false} // no comma here
             ],
-            "correct": "<p><span>You got it - who could forget those good ol' days of Communist witchhunts? Totally justified.</span> <a href='http://tfninsider.org/2009/10/21/more-mcleroy-malarkey-part-1/' target='_blank'>McLeroy circulated a note to the U.S. History curriculum panel stating</a>: &#34;Read the latest on McCarthy – he was basically vindicated.&#34; How he was vindicated was not detailed by McLeroy.</p>",
-            "incorrect": "<p><span>C'mon, where's your support for freedom-loving non-Commies?</span> <a href='http://tfninsider.org/2009/10/21/more-mcleroy-malarkey-part-1/' target='_blank'>McLeroy circulated a note to the U.S. History curriculum panel stating</a>: &#34;Read the latest on McCarthy – he was basically vindicated.&#34; How he was vindicated was not detailed by McLeroy.</p>" // no comma here
+            "correct": "<p><span>You got it - who could forget those good ol' days of Communist witch-hunts?</span> McLeroy circulated a note to the U.S. History curriculum panel stating: &#34;Read the latest on McCarthy – he was basically vindicated.&#34; How he was vindicated was not detailed by McLeroy.</p>",
+            "incorrect": "<p><span>C'mon, who doesn't love exposing Commies?</span> McLeroy circulated a note to the U.S. History curriculum panel stating: &#34;Read the latest on McCarthy – he was basically vindicated.&#34; How he was vindicated was not detailed by McLeroy.</p>" // no comma here
         },
         { // Question 4
             "q": "The new history standards that the board passed in 2010 instruct students that international treaties  _______________.",
             "a": [
-                {"option": "Represent a necessary part of our increasingly global world.",    "correct": false},
-                {"option": "Are a helpful tool in diplomacy.",     "correct": false},
+                {"option": "Are a necessary part of our increasingly global world.",    "correct": false},
+                {"option": "Provide a helpful tool in diplomacy.",     "correct": false},
                 {"option": "Undermine U.S. sovereignty.",      "correct": true} // no comma here
             ],
             "correct": "<p><span>Heck yeah - Texas should just secede from the world!</span> In a last minute change to the social studies curriculum standards, the &#34;black helicopter&#34; crowd at the SBOE inserted this paranoid attack on international cooperation: &#34;evaluate efforts by global organizations to undermine U.S. sovereignty through the use of treaties.&#34;</p>",
             "incorrect": "<p><span>Ha, you probably did Model UN.</span> In a last minute change to the social studies curriculum standards, the &#34;black helicopter&#34; crowd at the SBOE inserted this paranoid attack on international cooperation: &#34;evaluate efforts by global organizations to undermine U.S. sovereignty through the use of treaties.&#34;</p>" // no comma here
         },
         { // Question 5
-            "q": "True or False: Dinosaurs were present on Noah's Ark.",
+            "q": "True or False: Dinosaurs were present on Noah's Ark. (According to an SBOE member.)",
             "a": [
                 {"option": "True",    "correct": true},
                 {"option": "False",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Yup, and the velicoraptors kept things very interesting for those 150 days.</span> In the documentary “The Revisionaries”, former SBOE chair Don McLeroy proudly announces to children in his Sunday School class that dinosaurs were present on Noah’s Ark. (Even the six year old kids in his class seemed skeptical.)</p>",
+            "correct": "<p><span>Yup, and the velicoraptors kept things very interesting for those 40 days and 40 nights.</span> In the documentary “The Revisionaries”, former SBOE chair Don McLeroy proudly announces to children in his Sunday School class that dinosaurs were present on Noah’s Ark. (Even the six year old kids in his class seemed skeptical.)</p>",
             "incorrect": "<p><span>Whoa there, Darwin, got any <em>proof</em>?</span> In the documentary “The Revisionaries”, former SBOE chair Don McLeroy proudly announces to children in his Sunday School class that dinosaurs were present on Noah’s Ark. (Even the six year old kids in his class seemed skeptical.)</p>" // no comma here
         },
         { // Question 6
